6bc493cc091a3ddb216766be97dd19d36ae3c3b8
Marina <mymuse@mail.ru>
2026-02-07 19:38:50 +0300

    hw

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import ru.hogwarts.school.exceptions.NoStudentsException;
 import ru.hogwarts.school.exceptions.NoSuchStudentException;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.AvatarRepository;
@@ -12,6 +13,8 @@ import ru.hogwarts.school.repository.StudentRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
@@ -32,16 +35,11 @@ public class StudentService {
 
     public Student findStudent(Long id) {
         logger.debug("Finding student method invoked");
-        try {
-            return studentRepository.findById(id).orElseThrow(()-> new NoSuchStudentException(id));
-        } catch (NoSuchStudentException e) {
-            logger.error("No student with id {}", id, e);
-            throw e;
-        }
+            return studentRepository.findById(id).orElseThrow(() -> new NoSuchStudentException(id));
     }
 
     public List<Student> findAllStudents() {
-        logger.info("Getting all students method invoked");
+        logger.debug("Getting all students method invoked");
         return studentRepository.findAll();
     }
 
@@ -49,12 +47,7 @@ public class StudentService {
         logger.info("Editing student method invoked");
         Optional<Student> existingStudent = studentRepository.findById(student.getId());
         if (existingStudent.isEmpty()) {
-            try {
-                throw new NoSuchStudentException(student.getId());
-            } catch (NoSuchStudentException e) {
-                logger.error("No such student", e);
-                throw e;
-            }
+            throw new NoSuchStudentException(student.getId());
         }
         return studentRepository.save(student);
     }
@@ -64,19 +57,14 @@ public class StudentService {
         logger.info("Removing student method invoked");
         Optional<Student> studentOptional = studentRepository.findById(id);
         if (studentOptional.isEmpty()) {
-            try {
-                throw new NoSuchStudentException(id);
-            } catch (NoSuchStudentException e) {
-                logger.error("No student with id {}", id, e);
-                throw e;
-            }
+            throw new NoSuchStudentException(id);
         }
         avatarRepository.deleteByStudentId(id);
         studentRepository.deleteById(id);
     }
 
     public List<Student> findByAge(int age) {
-        logger.info("Finding student by name method invoked");
+        logger.debug("Finding student by age method invoked");
         return studentRepository.findByAge(age);
     }
 
@@ -103,5 +91,26 @@ public class StudentService {
     public List<Student> getFiveLastStudents() {
         logger.info("Getting last five students method invoked");
         return studentRepository.getFiveLastStudents();
+    }
+
+    public List<String> getStudentNamesStartingWithA() {
+        return studentRepository.findAll()
+                .stream()
+                .map(student -> student.getName().toUpperCase())
+                .filter(name -> name.startsWith("A"))
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
+    }
+
+    public double getAverageAge() {
+        OptionalDouble averageAge = studentRepository.findAll()
+                .stream()
+                .mapToInt(Student::getAge)
+                .average();
+        if (averageAge.isPresent()) {
+            return averageAge.getAsDouble();
+        } else {
+            throw new NoStudentsException();
+        }
     }
 }
